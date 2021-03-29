734a7c8867d3f9b5f9c3209a1e6eaf62a328337a
Edwin Carlsson <edwin98carlsson@gmail.com>
2021-03-29 14:03:44 +0200

    Tests for argmad

--- src/test/java/utilities/MathUtilitiesTest.java
+++ src/test/java/utilities/MathUtilitiesTest.java
@@ -11,10 +11,17 @@ public class MathUtilitiesTest {
 	}
 
 	@Test
-	public void argMax() {
-		double[] vals = {0.1, 0.2, -1000, 1000,
-			0}; // test big negatives, small positives and big positives
+	public void argMaxHappy() {
+		double[] vals = {0.1, 0.2, -1000, 1000, 0};
 		assertEquals(3, MathUtilities.argMax(vals));
 		assertEquals(1000, MathUtilities.max(vals), 1e-9);
+	}
+
+	@Test
+	public void argMaxUnhappy() {
+		double[] vals = {1, 2, 3, 4, 5, 1000};
+		double[] vals0 = {1000, 1, 2, 3, 4, 5};
+		assertEquals(vals.length - 1, MathUtilities.argMax(vals));
+		assertEquals(0, MathUtilities.argMax(vals0));
 	}
 }
